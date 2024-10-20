d08786e4265022ee879fac9fc5aa41f6b0d711ee
mb-1208 <mbntngfadlillahh1212@gmail.com>
2024-10-21 02:55:41 +0700

    Change project name on comingsoon page

--- src/app/comingsoon/page.tsx
+++ src/app/comingsoon/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Page() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center relative px-4">
-      <h1 className="text-xl font-semibold border rounded-full border-[#232222] px-4 py-1">Unlisted Rental</h1>
+      <h1 className="text-xl font-semibold border rounded-full border-[#232222] px-4 py-1">Suaka</h1>
       <h1 className="text-4xl md:text-5xl font-bold my-4 uppercase">
         Coming Soon
       </h1>
